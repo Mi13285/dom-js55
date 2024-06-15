b0cd57a2fcf056dab797aa8d3094c9d30bd4783e
Mi13285 <mixkud95@inbox.ru>
2024-06-15 19:22:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+(function () {
+  document.querySelector(".nav-button").addEventListener(
+    "click",
+    function () {
+      this.parentNode.parentNode.classList.toggle("closed");
+    },
+    false
+  );
+})();
